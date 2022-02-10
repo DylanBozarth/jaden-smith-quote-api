6a40fba4bc40b082a84cb39fcdc12a3839dac374
dylan <dylanabozarth@gmail.com>
2022-02-09 19:24:48 -0800

    requst

---
+++ index.js
@@ -0,0 +1,10 @@
+const quotes = require("./quotes.json");
+
+const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET",
+    "Access-Control-Allow-Headers": "Content-Type",
+};
+
+
+
