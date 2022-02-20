f4802bbfc6347a48ff08872737e51108076c19a9
Dylan Bozarth <dylanabozarth@gmail.com>
2022-02-19 19:06:46 -0800

    quotes are working

--- index.js
+++ index.js
@@ -7,4 +7,38 @@ const headers = {
 };
 
 
+const express = require('express')
+const app = express()
+const port = 4000
 
+app.get('/', (req, res) => {
+  
+  const quote = quotes[Math.floor(Math.random() * quotes.length)];
+  res.send(quote)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+/*
+async function handleRequest(request) {
+    try {
+        const quote = quotes[Math.floor(Math.random() * quotes.length)];
+
+        const json = JSON.stringify({ quote: quote });
+
+        return new Response(json, {
+            headers: { ...headers, "content-type": "application/json" },
+        });
+    } catch (error) {
+        return new Response("An unexpected error ocurred", {
+            status: 500,
+            headers: { ...headers },
+        });
+    }
+}
+
+addEventListener("fetch", (event) => {
+    event.respondWith(handleRequest(event.request));
+});
+*/
